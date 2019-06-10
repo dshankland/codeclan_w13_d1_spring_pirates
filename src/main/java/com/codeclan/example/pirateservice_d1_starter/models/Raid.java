04cb2e3f82f514a4df0f00cc0db0ae84f5a6db92
Darren Shankland <ds@4pq.co.uk>
2019-06-10 21:43:53 +0100

    initial commit

---
+++ src/main/java/com/codeclan/example/pirateservice_d1_starter/models/Raid.java
@@ -0,0 +1,84 @@
+package com.codeclan.example.pirateservice_d1_starter.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "raids")
+public class Raid {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String location;
+
+    @Column
+    private int loot;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "pirate_raid",
+            joinColumns = { @JoinColumn(
+                    name = "raid_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "pirate_id",
+                    nullable = false,
+                    updatable = false)
+            })
+    @Column
+    private List<Pirate> pirates;
+
+    public Raid(String location, int loot) {
+        this.location = location;
+        this.loot = loot;
+        this.pirates = new ArrayList<Pirate>();
+    }
+
+    public Raid() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getLoot() {
+        return loot;
+    }
+
+    public void setLoot(int loot) {
+        this.loot = loot;
+    }
+
+    public List<Pirate> getPirates() {
+        return pirates;
+    }
+
+    public void setPirates(List<Pirate> pirates) {
+        this.pirates = pirates;
+    }
+
+    public void addPirate(Pirate pirate) {
+        this.pirates.add(pirate);
+    }
+}
